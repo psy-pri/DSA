e6897e3024d926a66abcf5199b2b8cd4baba878a
Priyanka Shukla <priyankashukla769@gmail.com>
2022-08-31 20:14:12 +0530

    add func

--- LinkedList.py
+++ LinkedList.py
@@ -45,9 +45,66 @@ class LinkedList:
             self.tail = new_node
             #update list length 
             self.length += 1
-   
+
+    def prepend(self,value):
+        #create node
+        new_node = Node(value)
+        #point new node to existing head 
+        new_node.next = self.head 
+        #assign new node as head node 
+        self.head = new_node 
+    
+    def pop(self):
+        #temp - holds current node
+        #pre - holds previous node 
+        temp = self.head
+        pre = self.head 
+        #traverse till next is not none
+        while temp.next is not None:
+            pre = temp 
+            temp = temp.next
+        #assign pre node as tail node 
+        self.tail = pre 
+        #assign next as None for tail node
+        self.tail.next = None 
+        return temp 
+
+    def popfirst(self):
+        temp = self.head
+        #assign next node of head as head node 
+        self.head = self.head.next 
+        #update next value for old head
+        temp.next = None
+        #update list length 
+        self.length -= 1
+        return temp 
+
+    def get(self,index):
+        if index < 0 or index >= self.length:
+            return None 
+        else:
+            temp = self.head 
+            for _ in range(index):
+                temp = temp.next
+            return temp.value #for get
+            #return temp - for set 
+
+    def set_value(self,index,value):
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value 
+            return True 
+        return False
+            
+                
+
 new_linked_list = LinkedList(1)
 # print(new_linked_list.head.value)
 new_linked_list.append(2)
+new_linked_list.prepend(0)
+# new_linked_list.pop()
+# new_linked_list.popfirst()
+print(new_linked_list.get(0))
+new_linked_list.set_value(0,5)
 new_linked_list.print_list()
 
